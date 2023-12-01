bc5962e9a8ab960753638e105231c71c0f868ad1
Gabriellm-dev <gabriellm.dev@gmail.com>
2023-12-01 10:57:13 -0300

    user entity and resources

---
+++ src/main/java/com/glm/springjpahiber/SpringjpahiberApplication.java
@@ -0,0 +1,13 @@
+package com.glm.springjpahiber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjpahiberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjpahiberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glm/springjpahiber/SpringjpahiberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glm.springjpahiber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjpahiberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
